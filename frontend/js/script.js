53fb9d29a72e9171a1c8da0aa9cc3a7fa06866c0
TaeBbong <mok05289@korea.ac.kr>
2019-12-27 14:19:24 +0900

    Init routing

---
+++ frontend/js/script.js
@@ -0,0 +1,19 @@
+var API_URL = "";
+
+$(function () {
+    $('summary').click(function () {
+        console.log('summary');
+        // $.get(API_URL, {}, function (data) {
+        //     console.log(data);
+        // });
+    });
+});
+
+$(function () {
+    $('complete').click(function () {
+        console.log('complete');
+        // $.get(API_URL, {}, function (data) {
+        //     console.log(data);
+        // });
+    });
+});
